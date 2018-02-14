7251b6a32119f0a3e1500c06bae195502806663c
Simon Márton <simon.marton@gmail.com>
2018-02-14 16:05:42 +0100

    chore(server): Use PORT envvar if provided.

--- src/server.js
+++ src/server.js
@@ -7,37 +7,41 @@ const { graphqlKoa, graphiqlKoa } = require('apollo-server-koa');
 const schema = require('./graphql');
 const mongoConnector = require('./mongo-connector');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-const getBearerToken = token => token ? token.replace(/^Bearer\s*/, '') : null;
+const getBearerToken = token =>
+  token ? token.replace(/^Bearer\s*/, '') : null;
 
 const start = async () => {
   const app = new Koa();
   const router = new KoaRouter();
-  
+
   const mongo = await mongoConnector();
-  
-  router.post('/graphql', graphqlKoa(({ request: { header: { authorization } } }) => {
-    return {
-      schema,
-      context: { mongo, authorization: getBearerToken(authorization) }
-    };
-  }));
+
+  router.post(
+    '/graphql',
+    graphqlKoa(({ request: { header: { authorization } } }) => {
+      return {
+        schema,
+        context: { mongo, authorization: getBearerToken(authorization) }
+      };
+    })
+  );
   router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
-  
-  app.use(bodyParser({
-    extendTypes: {
-      // json: ['text/plain']
-    }
-  }));
+
+  app.use(
+    bodyParser({
+      extendTypes: {
+        // json: ['text/plain']
+      }
+    })
+  );
   app.use(cors());
-  
-  app
-    .use(router.routes())
-    .use(router.allowedMethods());
-  
+
+  app.use(router.routes()).use(router.allowedMethods());
+
   app.listen(PORT);
-  
+
   console.log(`Server listening on localhost:${PORT}`);
 };
 
