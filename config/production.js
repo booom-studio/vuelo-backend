a42b3764f89e0d324e07b92079f394a03031e9f0
Simon Márton <simon.marton@gmail.com>
2018-02-14 17:14:38 +0100

    config(heroku)

---
+++ config/production.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dbUrl: process.env.MONGODB_URI,
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: '30m'
+  }
+};
